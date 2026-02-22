8baad9d467f8ef6d9bc7c66c71e54f13207b3ed0
=shaikhanish123 <=anishahamadf@gmail.com>
2026-02-22 14:13:16 +0530

    something update in oops

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+String name;
+String rollno;
+String branch;
+public static void main(String[] args){
+    Student s1=new Student();
+s1.name="shaikh";
+s1.rollno="12";
+s1.branch="computers";
+System.out.println(s1.name+" "+s1.rollno+" "+s1.branch);
+}
+}
